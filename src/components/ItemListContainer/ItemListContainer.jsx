4d203dba2e2b1cf902c1e734f2a5b7fdf85ee353
Catalina Lima Brun <catalinalbrun@gmail.com>
2023-05-04 19:50:52 -0300

    preentrega1

---
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,9 @@
+const ItemListContainer = ({greetings}) => {
+    return (
+        <div>
+            <h1>BIENVENIDOS A CALMER!</h1>
+        </div>
+    )
+}
+
+export default ItemListContainer
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import CartWidget from "../CartWidget/CartWidget"
+
+export default function NavBar(){
+    return (
+        <nav>
+            <img src={calmer} alt="logo" />
+            <ul>
+                <li>
+                    <a href="/">INICIO</a>
+                </li>
+                <li>
+                    <a href="/">LIBROS</a>
+                </li>
+                <li>
+                    <a href="/">CAFETERIA</a>
+                </li>
+                <li>
+                    <a href="/">CONTACTO</a>
+                </li>
+            </ul>
+            <CartWidget/>
+        </nav>
+    )
+}
